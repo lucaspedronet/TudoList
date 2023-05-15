6f38c3d251d5928f8487ed6e765473a953343ff8
Shisui <cauadasilva555@gmail.com>
2023-05-15 07:56:43 -0300

    Branch

--- App.tsx
+++ App.tsx
@@ -1,118 +1,211 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Alert,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App() {
+  const [name, setName] = useState('');
+  const [members, setMembers] = useState<string[]>([]);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  function handleSubmit() {
+    if (name.length <= 0) {
+      return;
+    }
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+    // if (members.includes(name.trim())) {
+    //   return Alert.alert(
+    //     'Ateção',
+    //     'O participante: ' + name + ' já foi adicionado!',
+    //   );
+    // }
+
+    setMembers(parms => [name, ...parms]);
+
+    setName('');
+  }
+
+  function onMemberDestroi(memberDetroi: string) {
+    const newMembers = members.filter(m => m !== memberDetroi);
+
+    setMembers(newMembers);
+  }
+
+  function handleRemoveMember(member: string) {
+    Alert.alert('Remover', 'Remover o ' + member, [
+      {
+        
+        onPress: () => {
+          onMemberDestroi(member);
+          console.log('removeu');
+        },
+      },
+    ]);
+  }
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
+    <View style={styled.container}>
+      <Text key={1} style={styled.titleEvent}>
+        to do
       </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
+      
+
+      <View style={styled.containerInput}>
+        <TextInput
+          placeholder="Adicione uma nova tarefa"
+          placeholderTextColor="#6B6B6B"
+          style={styled.input}
+          onChangeText={setName}
+          value={name}
+        />
+
+        <TouchableOpacity style={styled.buttonAdd} onPress={handleSubmit}>
+          <Text style={styled.labelButton}>o</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={styled.check}>
+        Criadas:  Concluidas:
       </Text>
+      
+      <Text style={styled.lineLabel}>
+        
+      </Text>
+
+
+
+      {members.length > 0 ? (
+        members.map((name, index) => (
+          <View key={name + index} style={styled.member}>
+            <Text style={styled.memberLabel}>{name}</Text>
+
+            <TouchableOpacity
+              style={styled.buttonRemove}
+              onPress={() => handleRemoveMember(name)}>
+              <Text style={styled.labelButton}>-</Text>
+            </TouchableOpacity>
+          </View>
+        ))
+      ) : (
+        <Text key={4} style={styled.paragraph}>
+          Você ainda não tem tarefas cadastradas. Crie tarefas e organize seus itens a fazer
+        </Text>
+        
+      )}
     </View>
   );
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to aesgAWg see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
+const styled = StyleSheet.create({
+  container: {
+    backgroundColor: 'black',
+    flex: 1,
     paddingHorizontal: 24,
+    paddingVertical: 25,
   },
-  sectionTitle: {
+  titleEvent: {
+    color: 'blue',
     fontSize: 24,
-    fontWeight: '600',
+    lineHeight: 28.13,
+    fontWeight: '700',    
+    textAlign: 'center',
+    flexDirection: 'row',
+    alignItems: 'center', 
+    gap: 7,   
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
+  titleMembers: {
+    color: '#FDFCFE',
+    fontSize: 20,
+    lineHeight: 23.44,
+    fontWeight: '700',
+    marginTop: 42,
+  },
+  paragraph: {
+    color: '#FDFCFE',
+    fontSize: 14,
+    lineHeight: 16.41,
+    fontWeight: '400',
+    marginTop: 42,
+    textAlign: 'center',
+    paddingHorizontal: 30,
+  },
+  check: {
+    color: 'blue',
+    lineHeight: 18.75,
+    fontSize: 16,
+    fontWeight: '400',
+    flexDirection: 'row',
+    marginTop: 34,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    height: 56,
+    borderRadius: 4,
+    backgroundColor: '#1F1E25',
+    color: '#FDFCFE',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    fontSize: 14,
+    lineHeight: 20,
+    fontWeight: '400',
+    borderColor: 'blue',
+    borderWidth: 1
+  },
+  containerInput: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 34,
+    gap: 7,
+  },
+  buttonAdd: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 56,
+    height: 56,
+    borderRadius: 4,
+    backgroundColor: 'blue',
+  },
+  buttonRemove: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 56,
+    height: 56,
+    borderRadius: 4,
+    backgroundColor: 'gray',
+  },
+  labelButton: {
+    color: '#fff',
+    fontSize: 24,
+    lineHeight: 24,
     fontWeight: '400',
   },
-  highlight: {
-    fontWeight: '700',
+  member: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingStart: 16,
+    width: '85%',
+    height: 60,
+    marginTop: 16,
+    borderRadius: 4,
+    backgroundColor: '#1F1E25',
+  },
+  memberLabel: {
+    color: '#fff',
+    fontSize: 16,
+    lineHeight: 18.75,
+  },
+  lineLabel: {
+    fontSize: 16,
+    borderTopWidth: 3,
+    borderColor: 'gray',
+    paddingBottom:4,
+    marginTop:20,
   },
 });
-
-export default App;
