70aa5b6ae75d15ee553598504721362f00fdeaab
Shisui <cauadasilva555@gmail.com>
2023-05-16 21:45:21 -0300

    aula 16/05

--- App.tsx
+++ App.tsx
@@ -1,211 +1,334 @@
 import React, {useState} from 'react';
 import {
   Text,
+  TextInput,
   View,
   StyleSheet,
-  TextInput,
   TouchableOpacity,
   Alert,
+  Image,
 } from 'react-native';
 
+const itemsImg = {
+  logo: require('./assents/logo.png'),
+  clipboard: require('./assents/clipboard.png'),
+  delete: require('./assents/delete.png'),
+  checked: require('./assents/checked.png'),
+  circulo: require('./assents/icon-circulo.png'),
+};
+
+type TarefaType = {
+  id: string;
+  nome: string;
+  feito: boolean;
+};
+
 export default function App() {
-  const [name, setName] = useState('');
-  const [members, setMembers] = useState<string[]>([]);
+  const [tarefa, setTarefa] = useState('');
+  const [listaDeTarefas, setListaDeTarefas] = useState<TarefaType[]>([]);
 
   function handleSubmit() {
-    if (name.length <= 0) {
-      return;
+    if (!tarefa) {
+      return Alert.alert('Por favor digite uma tarefa!');
     }
 
-    // if (members.includes(name.trim())) {
-    //   return Alert.alert(
-    //     'Ateção',
-    //     'O participante: ' + name + ' já foi adicionado!',
-    //   );
-    // }
+    const tarefaExiste = listaDeTarefas.some(t => t.nome === tarefa);
 
-    setMembers(parms => [name, ...parms]);
+    if (tarefaExiste) {
+      return Alert.alert(
+        'Esta tarefa já existe em sua agenda',
+        'Por favor digite outra tarefa!',
+      );
+    }
 
-    setName('');
+    const novaTarefa: TarefaType = {
+      feito: false,
+      id: Math.random().toString().split('.')[1],
+      nome: tarefa,
+    };
+
+    setListaDeTarefas(m => [novaTarefa, ...m]);
+    setTarefa('');
   }
-
-  function onMemberDestroi(memberDetroi: string) {
-    const newMembers = members.filter(m => m !== memberDetroi);
-
-    setMembers(newMembers);
-  }
-
-  function handleRemoveMember(member: string) {
-    Alert.alert('Remover', 'Remover o ' + member, [
-      {
-        
-        onPress: () => {
-          onMemberDestroi(member);
-          console.log('removeu');
+  function handleDelete(id: string) {
+    Alert.alert(
+      'REMOVER',
+      'Tem certeza que deseja excluir a tarefa?',
+      [
+        {
+          text: 'Não',
+          style: 'cancel',
         },
-      },
-    ]);
+        {
+          text: 'Sim',
+          onPress: () => {
+            setListaDeTarefas(props => {
+              return props.filter(t => t.id !== id);
+            });
+          },
+        },
+      ],
+      {cancelable: false},
+    );
+  }
+
+  function handleToggle(id: string) {
+    setListaDeTarefas(props => {
+      return props.map(t => (t.id === id ? {...t, feito: !t.feito} : t));
+    });
+  }
+
+  function onTarefaconcluidas()
+  {
+    const newTarefasConcluidas = listaDeTarefas.filter(t=>t.feito);
   }
 
   return (
     <View style={styled.container}>
-      <Text key={1} style={styled.titleEvent}>
-        to do
-      </Text>
-      
-
-      <View style={styled.containerInput}>
-        <TextInput
-          placeholder="Adicione uma nova tarefa"
-          placeholderTextColor="#6B6B6B"
-          style={styled.input}
-          onChangeText={setName}
-          value={name}
-        />
-
-        <TouchableOpacity style={styled.buttonAdd} onPress={handleSubmit}>
-          <Text style={styled.labelButton}>o</Text>
-        </TouchableOpacity>
+      <View style={styled.header}>
+        <Image style={styled.logo} source={itemsImg.logo} />
       </View>
-
-      <Text style={styled.check}>
-        Criadas:  Concluidas:
-      </Text>
-      
-      <Text style={styled.lineLabel}>
-        
-      </Text>
-
-
-
-      {members.length > 0 ? (
-        members.map((name, index) => (
-          <View key={name + index} style={styled.member}>
-            <Text style={styled.memberLabel}>{name}</Text>
-
-            <TouchableOpacity
-              style={styled.buttonRemove}
-              onPress={() => handleRemoveMember(name)}>
-              <Text style={styled.labelButton}>-</Text>
-            </TouchableOpacity>
+      <View style={styled.content}>
+        <View style={styled.containerinput}>
+          <TextInput
+            placeholder="Adicione uma nova tarefa"
+            placeholderTextColor={'#808080'}
+            style={styled.input}
+            onChangeText={setTarefa}
+            value={tarefa}
+          />
+          <TouchableOpacity style={styled.buttonadd} onPress={handleSubmit}>
+            <Text key={1} style={styled.labelButton}>
+              +
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{flexDirection: 'row', height: 71}}>
+          <View style={{flex: 1, flexDirection: 'row', top: 32, height: 19}}>
+            <Text style={styled.criadas}>Criadas</Text>
+            <View
+              style={{
+                height: 19,
+                width: 25,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 999,
+                backgroundColor: '#333333',
+                left: 8,
+              }}>
+              <Text style={styled.numeroDeTarefas}>
+                {listaDeTarefas.length}
+              </Text>
+            </View>
           </View>
-        ))
-      ) : (
-        <Text key={4} style={styled.paragraph}>
-          Você ainda não tem tarefas cadastradas. Crie tarefas e organize seus itens a fazer
-        </Text>
-        
-      )}
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              top: 32,
+              height: 19,
+              justifyContent: 'flex-end',
+            }}>
+            <Text style={styled.concluidas}>Concluídas</Text>
+            <View
+              style={{
+                height: 19,
+                width: 25,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 999,
+                backgroundColor: '#333333',
+              }}>
+              <Text style={styled.numeroDeTarefas}>0</Text>
+            </View>
+          </View>
+        </View>
+        <View style={styled.contentlist}>
+          {listaDeTarefas.length === 0 ? (
+            <View style={{flex: 1, alignItems: 'center'}}>
+              <Image style={styled.clipboard} source={itemsImg.clipboard} />
+              <Text style={styled.title3}>
+                Você ainda não tem tarefas cadastradas{'\n'}
+                Crie tarefas e organize seus itens a fazer.
+              </Text>
+            </View>
+          ) : (
+            listaDeTarefas.map(tarefa => {
+              return (
+                <View key={tarefa.id} style={styled.containerlist}>
+                  <TouchableOpacity
+                    style={styled.botaoFeito}
+                    onPress={() => handleToggle(tarefa.id)}>
+                    <Image
+                      source={
+                        tarefa.feito ? itemsImg.checked : itemsImg.circulo
+                      }
+                    />
+                  </TouchableOpacity>
+                  <View style={styled.list}>
+                    <Text
+                      style={[
+                        styled.list,
+                        {
+                          textDecorationLine: tarefa.feito
+                            ? 'line-through'
+                            : 'none',
+                        },
+                      ]}>
+                      {tarefa.nome}
+                    </Text>
+                  </View>
+                  <TouchableOpacity
+                    style={styled.buttondelet}
+                    onPress={() => handleDelete(tarefa.id)}>
+                    <Image style={styled.delete} source={itemsImg.delete} />
+                  </TouchableOpacity>
+                </View>
+              );
+            })
+          )}
+        </View>
+      </View>
     </View>
   );
 }
 
 const styled = StyleSheet.create({
   container: {
-    backgroundColor: 'black',
+    flex: 1,
+  },
+  header: {
+    height: 143,
+    alignItems: 'center',
+    backgroundColor: '#0D0D0D',
+  },
+  logo: {
+    height: 32,
+    width: 110,
+    top: 40,
+  },
+  delete: {
+    height: 22,
+    width: 22,
+  },
+  botaoFeito: {
+    marginHorizontal: 16,
+    width: 20,
+    height: 20,
+  },
+  clipboard: {
+    height: 56,
+    width: 56,
+    top: 48,
+  },
+  content: {
+    backgroundColor: '#1A1A1A',
     flex: 1,
     paddingHorizontal: 24,
-    paddingVertical: 25,
   },
-  titleEvent: {
-    color: 'blue',
+  title: {
+    color: '#FDFCFE',
     fontSize: 24,
     lineHeight: 28.13,
-    fontWeight: '700',    
-    textAlign: 'center',
-    flexDirection: 'row',
-    alignItems: 'center', 
-    gap: 7,   
-  },
-  titleMembers: {
-    color: '#FDFCFE',
-    fontSize: 20,
-    lineHeight: 23.44,
     fontWeight: '700',
-    marginTop: 42,
   },
-  paragraph: {
+  criadas: {
+    color: '#4EA8DE',
+    top: 3,
+    fontSize: 14,
+    lineHeight: 16.94,
+    fontWeight: '700',
+  },
+  numeroDeTarefas: {
+    color: '#D9D9D9',
+    textAlign: 'center',
+    fontSize: 12,
+    lineHeight: 14.52,
+    fontWeight: '700',
+  },
+  concluidas: {
+    color: '#8284FA',
+    top: 2,
+    right: 8,
+    fontSize: 14,
+    lineHeight: 16.94,
+    fontWeight: '700',
+  },
+  contentlist: {
+    flex: 1,
+    alignSelf: 'stretch',
+    borderTopWidth: 1,
+    borderColor: '#333333',
+    gap: 16,
+  },
+  title3: {
+    flex: 1,
     color: '#FDFCFE',
     fontSize: 14,
-    lineHeight: 16.41,
-    fontWeight: '400',
-    marginTop: 42,
+    lineHeight: 19.6,
     textAlign: 'center',
-    paddingHorizontal: 30,
-  },
-  check: {
-    color: 'blue',
-    lineHeight: 18.75,
-    fontSize: 16,
     fontWeight: '400',
-    flexDirection: 'row',
-    marginTop: 34,
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    top: 64,
+  },
+  subtitle: {
+    color: '#6B6B6B',
+    fontSize: 16,
+    lineHeight: 18.75,
+    fontWeight: '400',
   },
   input: {
     flex: 1,
-    height: 56,
-    borderRadius: 4,
-    backgroundColor: '#1F1E25',
-    color: '#FDFCFE',
+    height: 54,
+    borderRadius: 6,
+    borderWidth: 1,
+    borderColor: '#0D0D0D',
+    backgroundColor: '#262626',
+    color: '#F2F2F2',
     alignItems: 'center',
     paddingHorizontal: 16,
-    fontSize: 14,
-    lineHeight: 20,
-    fontWeight: '400',
-    borderColor: 'blue',
-    borderWidth: 1
+    fontSize: 16,
   },
-  containerInput: {
+  list: {
+    flex: 1,
+    height: 64,
+    textAlignVertical: 'center',
+    color: '#FDFCFE',
+    fontSize: 12,
+    fontWeight: '400',
+  },
+  containerinput: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginTop: 34,
-    gap: 7,
+    marginTop: -27,
+    gap: 4,
   },
-  buttonAdd: {
+  containerlist: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#333333',
+    backgroundColor: '#262626',
+  },
+  buttonadd: {
     alignItems: 'center',
     justifyContent: 'center',
-    width: 56,
-    height: 56,
-    borderRadius: 4,
-    backgroundColor: 'blue',
+    width: 54,
+    height: 54,
+    borderRadius: 6,
+    backgroundColor: '#1E6F9F',
   },
-  buttonRemove: {
+  buttondelet: {
     alignItems: 'center',
     justifyContent: 'center',
-    width: 56,
-    height: 56,
-    borderRadius: 4,
-    backgroundColor: 'gray',
+    width: 40,
+    height: 40,
+    marginHorizontal: 7,
   },
   labelButton: {
     color: '#fff',
     fontSize: 24,
     lineHeight: 24,
     fontWeight: '400',
-  },
-  member: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingStart: 16,
-    width: '85%',
-    height: 60,
-    marginTop: 16,
-    borderRadius: 4,
-    backgroundColor: '#1F1E25',
-  },
-  memberLabel: {
-    color: '#fff',
-    fontSize: 16,
-    lineHeight: 18.75,
-  },
-  lineLabel: {
-    fontSize: 16,
-    borderTopWidth: 3,
-    borderColor: 'gray',
-    paddingBottom:4,
-    marginTop:20,
   },
 });
